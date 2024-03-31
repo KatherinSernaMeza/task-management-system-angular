301c4caae4bf575ac625cb8f3f82c9c9b1e86a61
Katherin Serna <katherin.serna.m@gmail.com>
2024-03-30 21:27:43 -0700

    Set up Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
+import { HomeComponent } from './components/home/home.component';
+import { PlaylistsComponent } from './components/playlists/playlists.component';
+import { PlaylistDetailComponent } from './components/playlist-detail/playlist-detail.component';
+import { CreatePlaylistComponent } from './components/create-playlist/create-playlist.component';
+import { EditPlaylistComponent } from './components/edit-playlist/edit-playlist.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'playlist', component: PlaylistsComponent },
+  { path: 'playlist-detail/:id', component: PlaylistDetailComponent },
+  { path: 'create-playlist', component: CreatePlaylistComponent },
+  { path: 'edit-playlist/:id', component: EditPlaylistComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
---
+++ src/app/components/edit-playlist/edit-playlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-playlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-playlist.component.html',
+  styleUrl: './edit-playlist.component.scss'
+})
+export class EditPlaylistComponent {
+
+}
